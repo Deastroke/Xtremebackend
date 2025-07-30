1077dd61c0150e2c4ff1b068b23c4e3bc1a064a5
Deastroke <163570672+Deastroke@users.noreply.github.com>
2025-07-29 18:44:38 -0600

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 4000;
+
+app.use(cors({
+  origin: 'https://xtremefront.onrender.com' // Cambia si tu dominio frontend es diferente
+}));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
